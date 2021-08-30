bbf052ce69da5234409ce7801b0772db8e44c8d2
Gabriel  S <gabrielmarioseno@gmail.com>
2021-08-30 20:32:50 -0300

    Creacion de estructura de proyecto, creacion de todas las clases

---
+++ src/paquete/Comparable.java
@@ -0,0 +1,7 @@
+package paquete;
+
+import java.util.Comparator;
+
+public class Comparable implements Comparator<T> {
+
+}
---
+++ src/paquete/PromocionAbsoluta.java
@@ -0,0 +1,5 @@
+package paquete;
+
+public class PromocionAbsoluta extends Promocion {
+
+}
